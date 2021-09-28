3c2d8334e46bccd804c924908ea2639f28bf9027
delmar <delmarpre@gmail.com>
2021-09-28 13:49:39 -0400

    first commit for loop display

---
+++ loop even or odd/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace loop_even_or_odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i<=100; i++)
+            {
+                Console.WriteLine(1)
+            }
+            if(if % 2 -- 0)
+            {
+                Console.WriteLine(1);
+            }
+
+
+        }
+    }
+    }
